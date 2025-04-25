43bbaca740fc80dc8a274e806f73676783eb7dff
Benjamin-Kabeya <bukasabenjamin6@gmail.com>
2025-04-26 00:09:39 +0100

    fix: change of header is horizontal padding

--- front-end/src/components/SearchForm.jsx
+++ front-end/src/components/SearchForm.jsx
@@ -8,7 +8,7 @@ const SearchForm = () => {
   
   return (
     <>
-        <form className={`flex justify-between px-2 py-0 rounded-md border 2xs:max-xs:w-52 xs:w-72 xs:max-md:w-72 dark:border-gray-100/15 xs:max-xsm:w-64 sm:max-[979px]:w-[450px] sm:max-[979px]:px-10`}>
+        <form className={`flex justify-between px-2 py-0 rounded-md border 2xs:max-xs:w-52 xs:w-72 xs:max-md:w-72 dark:border-gray-100/15 xs:max-xsm:w-64 sm:max-[979px]:w-[320px] `}>
         <input type="text" placeholder='Recherche...' className='outline-none 2xs:max-xs:w-full xs:max-md:w-80  md:flex-1 bg-transparent'/>
         <button className='w-10 h-10 flex justify-center items-center'>
             <Search />
