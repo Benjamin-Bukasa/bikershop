62601fbc315389a2ce2177353cc0f42a309b967e
Benjamin-Kabeya <bukasabenjamin6@gmail.com>
2025-04-30 11:47:54 +0100

    fix: change of ChooseUsItems component padding

--- front-end/src/components/ChooseUsItems.jsx
+++ front-end/src/components/ChooseUsItems.jsx
@@ -4,7 +4,7 @@ const ChooseUsItems = ({icon, title, description}) => {
   return (
     <>
         
-            <div className="bg-white dark:bg-zinc-950 rounded-lg shadow-lg flex flex-col justify-center items-center gap-5 py-10 text-center">
+            <div className="w-full md:w-1/3 bg-white dark:bg-zinc-950 rounded-lg shadow-lg flex flex-col justify-center items-center gap-5 py-10 text-center">
                 <div className="w-20 h-20 flex justify-center items-center text-red-600 rounded-full bg-red-200">
                     {icon}
                 </div>
